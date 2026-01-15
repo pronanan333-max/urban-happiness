d799ceb68a0401cb178156bc66665091c6a1f674
NeunG01 <ponanan001@hotmail.com>
2026-01-15 11:48:49 +0700

    up date

--- subplatform/settings.py
+++ subplatform/settings.py
@@ -84,7 +84,7 @@ AUTH_USER_MODEL = 'account.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
